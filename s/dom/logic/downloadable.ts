3c73a3ea25f1f2201e94e8cefb6e027607128786
Chase Moskal <chasemoskal@gmail.com>
2024-10-13 15:44:13 -0700

    tweak: syntax

--- s/dom/logic/downloadable.ts
+++ s/dom/logic/downloadable.ts
@@ -3,7 +3,10 @@ import {RenderResult} from "@benev/slate"
 import {Authfile} from "../../auth/file.js"
 import {Identity} from "../../auth/types.js"
 
-export function downloadable(identities: Identity[], fn: (filename: string, href: string) => RenderResult) {
+export function downloadable(
+		identities: Identity[],
+		fn: (filename: string, href: string) => RenderResult,
+	) {
 	const file = Authfile.fromIdentities(identities)
 	return fn(Authfile.name(file), Authfile.href(file))
 }
