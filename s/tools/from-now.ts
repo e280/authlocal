813dba683e4aaa9f4527c02696ff4d263034aa48
Chase Moskal <chasemoskal@gmail.com>
2024-11-15 03:38:15 -0800

    tweak: FromNow is now a const

--- s/tools/from-now.ts
+++ s/tools/from-now.ts
@@ -1,8 +1,8 @@
 
-export class FromNow {
-	static seconds = (n: number) => Date.now() + (n * 1000)
-	static minutes = (n: number) => Date.now() + (n * this.seconds(60))
-	static hours = (n: number) => Date.now() + (n * this.minutes(60))
-	static days = (n: number) => Date.now() + (n * this.hours(24))
+export const FromNow = {
+	seconds: (n: number) => Date.now() + (n * 1000),
+	minutes: (n: number) => Date.now() + (n * FromNow.seconds(60)),
+	hours: (n: number) => Date.now() + (n * FromNow.minutes(60)),
+	days: (n: number) => Date.now() + (n * FromNow.hours(24)),
 }
 
