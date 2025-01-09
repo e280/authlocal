914fefc0f7ac0e1e35cac1c11a5ac311f1a6f232
Chase Moskal <chasemoskal@gmail.com>
2025-01-09 01:49:52 -0800

    improve: popup centering

--- s/federated/utils/open-popup.ts
+++ s/federated/utils/open-popup.ts
@@ -1,6 +1,25 @@
 
 export function openPopup(url: string) {
-	const features = "width=700,height=800,resizable=yes,scrollbars=yes"
+	const width = 800
+	const height = 900
+
+	const centerX = window.screenX + (window.outerWidth / 2)
+	const centerY = window.screenY + (window.outerHeight / 2)
+
+	const left = centerX - (width / 2)
+	const top = centerY - (height / 2)
+
+	const features = [
+		`width=${width}`,
+		`height=${height}`,
+		`left=${left}`,
+		`top=${top}`,
+		`resizable=yes`,
+		`scrollbars=yes`,
+	].join(",")
+
+	console.log(features)
+
 	return window.open(url, "auth", features)
 }
 
