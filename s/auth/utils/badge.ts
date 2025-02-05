2141d3fdc7c4e0c96d56f0188289523f16ff0fb6
Chase Moskal <chasemoskal@gmail.com>
2025-02-05 15:54:24 -0800

    rework: badge implementation and formatting

--- s/auth/utils/badge.ts
+++ s/auth/utils/badge.ts
@@ -1,30 +1,55 @@
 
 import {Base58, Hex, Urname} from "@benev/slate"
 
+/**
+ * Badge is a human-friendly presentation format for arbitrary binary data.
+ *  - looks like "nomluc-rigpem.tg2bjNkjMh1H6M2b2EhD5V4x6XAqx9wyWddsBt"
+ *  - has an urname prefix 'preview'
+ *  - the rest of the data is in base58
+ *  - designed to be a nice way to present 256-bit passport thumbprints
+ *  - can be used for data of different lengths
+ *  - preview size can be customized as 'leadCount', which defaults to 4
+ */
 export class Badge {
-	readonly thumbprint: string
+	static readonly defaultLeadCount = 4
+	readonly hex: string
 	readonly string: string
 	readonly preview: string
 
-	constructor(public readonly bytes: Uint8Array) {
-		if (bytes.length !== 32)
-			throw new Error(`badge must be 32 bytes (got ${bytes.length})`)
-		this.thumbprint = Hex.string(this.bytes)
-		this.preview = Urname.string(this.bytes.slice(0, 4))
-		this.string = `${this.preview}-${Base58.string(this.bytes.slice(4))}`
+	constructor(
+			public readonly bytes: Uint8Array,
+			public readonly leadCount = Badge.defaultLeadCount,
+		) {
+		if (leadCount < 1)
+			throw new Error(`badge leadCount must be greater than 0 (was ${leadCount})`)
+		this.hex = Hex.string(this.bytes)
+		this.preview = Urname.string(this.bytes.slice(0, leadCount))
+		this.string = (bytes.length > leadCount)
+			? `${this.preview}.${Base58.string(this.bytes.slice(leadCount))}`
+			: this.preview
 	}
 
-	static fromString(badge: string) {
-		const [a, b, b58] = badge.split("-")
-		const appetizer = Urname.bytes([a, b].join("-"))
-		const entree = Base58.bytes(b58)
-		const bytes = new Uint8Array([...appetizer, ...entree])
-		return new this(bytes)
+	static parse(badge: string) {
+		const [urname, b58] = badge.split(".")
+
+		// badge is separated by a "."
+		if (b58) {
+			const appetizer = Urname.bytes(urname)
+			const entree = Base58.bytes(b58)
+			const bytes = new Uint8Array([...appetizer, ...entree])
+			return new this(bytes, appetizer.length)
+		}
+
+		// badge has no dot, and is all urname preview, no base58 part
+		else {
+			const bytes = Urname.bytes(urname)
+			return new this(bytes, bytes.length)
+		}
 	}
 
-	static fromThumbprint(hex: string) {
+	static fromHex(hex: string, leadCount = this.defaultLeadCount) {
 		const bytes = Hex.bytes(hex)
-		return new this(bytes)
+		return new this(bytes, leadCount)
 	}
 
 	toString() {
--- s/common/views/id/render-thumbprint.ts
+++ s/common/views/id/render-thumbprint.ts
@@ -3,7 +3,7 @@ import {IdView} from "./view.js"
 import {Badge} from "../../../auth/utils/badge.js"
 
 export const renderThumbprint = (thumbprint: string) => {
-	const badge = Badge.fromThumbprint(thumbprint)
+	const badge = Badge.fromHex(thumbprint)
 	return IdView([badge.string, badge.preview])
 }
 
