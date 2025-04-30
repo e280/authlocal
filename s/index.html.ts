3a185d65065c57b7357cfc3eef0fee5e581625ad
Chase Moskal <chasemoskal@gmail.com>
2025-04-29 22:18:18 -0700

    ui: new homepage design

--- s/index.html.ts
+++ s/index.html.ts
@@ -1,10 +1,11 @@
 
 import "@benev/slate/x/node.js"
 import shieldLockIcon from "./common/icons/tabler/shield-lock.icon.js"
-import {template, html, easypage, headScripts, git_commit_hash, read_file, unsanitized, renderSocialCard} from "@benev/turtle"
+import {template, html, easypage, headScripts, git_commit_hash, read_file, read_json, unsanitized, renderSocialCard} from "@benev/turtle"
 
 const domain = "authlocal.org"
 const favicon = "/assets/favicon.png"
+const version = (await read_json("package.json")).version
 
 export default template(async basic => {
 	const path = basic.path(import.meta.url)
@@ -18,6 +19,7 @@ export default template(async basic => {
 			<link rel="icon" href="${favicon}"/>
 			<style>${unsanitized(await read_file("x/manager/index.css"))}</style>
 			<meta data-commit-hash="${hash}"/>
+			<meta data-version="${version}"/>
 
 			${renderSocialCard({
 				themeColor: "#8800ff",
@@ -35,17 +37,25 @@ export default template(async basic => {
 			})}
 		`,
 		body: html`
-			<h1 class=title>
-				${html(shieldLockIcon.strings)}
-				<span>Authlocal.org</span>
-			</h1>
+			<header>
+				<div>
+					<h1>
+						${html(shieldLockIcon.strings)}
+						<span class=title>Authlocal.org</span>
+						<span class=version>v${version}</span>
+					</h1>
+					<p>No emails, no passwords, no databases</p>
+				</div>
+				<div>
+					<p><strong>User-sovereign login system</strong></p>
+					<p>Own your identity, privacy-focused, open-source</p>
+				</div>
+			</header>
 
 			<auth-manager></auth-manager>
 
 			<footer>
-				<p>User-sovereign, privacy-focused, open-source.</p>
-				<p>No emails, no passwords, no databases.</p>
-				<p>Learn more on <a href="https://github.com/authlocal/authlocal" target=_blank>GitHub</a>.</p>
+				<p>Learn more on <a href="https://github.com/authlocal/authlocal" target=_blank>GitHub</a></p>
 				<auth-safe-storage></auth-safe-storage>
 			</footer>
 		`,
