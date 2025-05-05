4c2f7e32d30beaec64ea44c20ec66fb251d5a28b
Chase Moskal <chasemoskal@gmail.com>
2025-05-05 01:57:29 -0700

    tweak: use tiny popup size

--- s/federated/parts/open-popup.ts
+++ s/federated/parts/open-popup.ts
@@ -1,7 +1,7 @@
 
 export function openPopup(url: string) {
-	const width = 800
-	const height = 800
+	const width = 420
+	const height = 640
 
 	const centerX = window.screenX + (window.outerWidth / 2)
 	const centerY = window.screenY + (window.outerHeight / 2)
