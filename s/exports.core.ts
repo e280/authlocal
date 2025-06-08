b52d1000cc56f5fd4bec12e37ae0ef3d40aa2c09
Chase Moskal <chasemoskal@gmail.com>
2025-06-08 15:21:42 -0700

    fix: exports for apps

--- s/exports.core.ts
+++ s/exports.core.ts
@@ -7,15 +7,8 @@
 
 export {Thumbprint, Bytename, Hex, Base58, sub, Sub} from "@e280/stz"
 
-// export {signClaim, verifyClaim, SignClaimOptions, VerifyClaimOptions, ClaimPayload} from "./core/core/claim/claims.js"
-// export {Nametag} from "./core/core/identity/identity.js"
-// export {Login, VerifyLoginOptions, LoginSignClaimOptions} from "./core/core/session/login.js"
-// export {Proof, verifyProof, VerifyProofOptions, ProofPayload} from "./core/core/session/proof.js"
-// export {Session} from "./core/core/session/session.js"
-// export {TokenParams, TokenPayload} from "./core/core/jwt/token.js"
-
+export * from "./trust/exports/app.js"
 export * from "./common/utils/id-hue.js"
 export * from "./common/utils/validation.js"
-
 export * from "./tools/time.js"
 
