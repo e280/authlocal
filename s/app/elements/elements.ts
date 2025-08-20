319366c8b1e946fbd7424122b5723f57d6277536
Chase Moskal <chasemoskal@gmail.com>
2025-08-20 15:26:09 -0700

    update: dependencies

--- s/app/elements/elements.ts
+++ s/app/elements/elements.ts
@@ -1,5 +1,5 @@
 
-import {Pipe} from "@e280/stz"
+import {pipe} from "@e280/stz"
 import {apply} from "@benev/slate"
 
 import {Auth} from "../auth.js"
@@ -7,8 +7,7 @@ import {provideAuth} from "./framework.js"
 import {AuthUser} from "./auth-user/element.js"
 import {AuthButton} from "./auth-button/element.js"
 
-export const appElements = (auth: Auth) => Pipe
-	.with({AuthButton, AuthUser})
+export const appElements = (auth: Auth) => pipe({AuthButton, AuthUser})
 	.to(provideAuth(auth))
 	.to(apply.reactive())
 	.done()
--- s/common/elements/elements.ts
+++ s/common/elements/elements.ts
@@ -1,11 +1,10 @@
 
-import {Pipe} from "@e280/stz"
+import {pipe} from "@e280/stz"
 import {apply} from "@benev/slate"
 
 import {AuthSigil} from "./auth-sigil/element.js"
 
-export const commonElements = Pipe
-	.with({AuthSigil})
+export const commonElements = pipe({AuthSigil})
 	.to(apply.reactive())
 	.done()
 
