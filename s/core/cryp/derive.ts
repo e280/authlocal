486277ee26b91d7a4f3857f93bcb2c1e2303e827
Chase Moskal <chasemoskal@gmail.com>
2026-01-19 23:21:53 -0800

    tweak: core naming

--- s/core/cryp/derive.ts
+++ s/core/cryp/derive.ts
@@ -2,34 +2,34 @@
 import {hex, txt} from "@e280/stz"
 import {ed25519, x25519} from "@noble/curves/ed25519.js"
 
-import {Id, Secret, Seed} from "./types.js"
 import {hashCat, keyBytes} from "./kit.js"
+import {Id, Secret, Seed} from "./types.js"
 
 export async function deriveId(secret: Secret): Promise<Id> {
 	const idBytes = ed25519.getPublicKey(keyBytes(secret))
 	return hex.fromBytes(idBytes)
 }
 
-export async function deriveSecret(seed: Seed, context: string) {
+export async function deriveSecret(seed: Seed, purpose: string) {
 	return hashCat(
 		keyBytes(seed),
-		txt.toBytes(context),
+		txt.toBytes(purpose),
 	)
 }
 
 export async function deriveSharedSecret(
-		aliceSecretHex: Secret,
-		bobIdHex: Id,
-		context: string,
+		aliceSecret: Secret,
+		bobId: Id,
+		purpose: string,
 	) {
 
-	const aliceXSecretBytes = ed25519.utils.toMontgomerySecret(hex.toBytes(aliceSecretHex))
-	const bobXPubBytes = ed25519.utils.toMontgomery(hex.toBytes(bobIdHex))
+	const aliceXSecretBytes = ed25519.utils.toMontgomerySecret(hex.toBytes(aliceSecret))
+	const bobXPubBytes = ed25519.utils.toMontgomery(hex.toBytes(bobId))
 	const shared = x25519.getSharedSecret(aliceXSecretBytes, bobXPubBytes)
 
 	return hashCat(
 		shared,
-		txt.toBytes(context),
+		txt.toBytes(purpose),
 	)
 }
 
