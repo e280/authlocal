053371006fc85f5ebfdebd012c21bf6215bc4292
Chase Moskal <chasemoskal@gmail.com>
2026-01-20 01:36:42 -0800

    add: alco session and proof types

--- s/core/alco/types.ts
+++ s/core/alco/types.ts
@@ -1,5 +1,5 @@
 
-import {Id, Keypair, Root} from "../cryp/types.js"
+import {Id, Keypair, Root, Secret} from "../cryp/types.js"
 
 /** public information about an identity */
 export type Profile = {
@@ -11,6 +11,25 @@ export type Profile = {
 export type Identity = {
 	root: Root
 	keypair: Keypair
+	profile: Profile
+}
+
+/** a successful login */
+export type Session = {
+
+	/** private key for this specific session */
+	secret: Secret
+
+	/** proves that this session is legit */
+	proofToken: string
+
+	/** derived secrets as requested from this login */
+	secrets: Secret[]
+}
+
+/** proof that a session was signed by the user's identity */
+export type Proof = {
+	sessionId: Id
 	profile: Profile
 }
 
