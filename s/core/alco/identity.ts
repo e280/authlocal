832203cb2c08b90d905c3b78533f6922e28e1338
Chase Moskal <chasemoskal@gmail.com>
2026-01-19 23:06:05 -0800

    replace: generateIdentity with deriveIdentity

--- s/core/alco/identity.ts
+++ s/core/alco/identity.ts
@@ -1,11 +1,15 @@
 
 import {hex} from "@e280/stz"
 import {Identity} from "./types.js"
+import {Seed} from "../cryp/types.js"
 import {moniker} from "../moniker/moniker.js"
 import {deriveId, deriveSecret} from "../cryp/derive.js"
 
-export async function generateIdentity(label?: string): Promise<Identity> {
-	const seed = hex.random(32)
+export async function deriveIdentity(
+		seed: Seed = hex.random(32),
+		label?: string,
+	): Promise<Identity> {
+
 	const secret = await deriveSecret(seed, "id")
 	const id = await deriveId(secret)
 
