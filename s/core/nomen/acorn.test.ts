52d97ba7327347afd1602eb8bcc90b74b5147cfc
Chase Moskal <chasemoskal@gmail.com>
2026-01-20 00:40:22 -0800

    improve: acorn tests

--- s/core/nomen/acorn.test.ts
+++ s/core/nomen/acorn.test.ts
@@ -12,11 +12,45 @@ nilfus
 `.trim()
 
 export default suite({
+	"root->acorn->root": test(async() => {
+		expect(acorn.toRoot(acorn(demoRoot))).is(demoRoot)
+	}),
+
 	"acorn matches demo": test(async() => {
 		expect(acorn(demoRoot)).is(demoAcorn)
 	}),
-	"root->acorn->root": test(async() => {
-		expect(acorn.toRoot(acorn(demoRoot))).is(demoRoot)
+
+	"tolerate whitespace": test(async() => {
+		expect(acorn.toRoot(`
+			namnut nibfer datset mitfer
+			mapfyl pidtem somsyt silrup
+			midsyr tacsul fopryn fadryl
+			milzod savdes macden sivwel
+			nilfus
+		`)).is(demoRoot)
+	}),
+
+	"tolerate lettercase": test(async() => {
+		expect(acorn.toRoot(`
+			namnut nibfer datset mitfer
+			MAPFYL PIDTEM SOMSYT SILRUP
+			midsyr tacsul fopryn fadryl
+			milzod savdes macden sivwel
+			nilfus
+		`)).is(demoRoot)
+	}),
+
+	"throw when checksum missing": test(async() => {
+		expect(() => acorn.toRoot(`
+			namnut nibfer datset mitfer
+			mapfyl pidtem somsyt silrup
+			midsyr tacsul fopryn fadryl
+			milzod savdes macden sivwel
+		`)).throws()
+	}),
+
+	"throw on empty": test(async() => {
+		expect(() => acorn.toRoot("")).throws()
 	}),
 })
 
