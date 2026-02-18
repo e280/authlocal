858bdd9159665848786f0947196878288dc80110
Chase Moskal <chasemoskal@gmail.com>
2026-02-17 16:33:43 -0800

    improve: delegate testing

--- s/core/alco/alco.test.ts
+++ s/core/alco/alco.test.ts
@@ -33,29 +33,71 @@ export default suite({
 		expect(() => verifyDelegate(delegate, {...allowed})).not.throws()
 	}),
 
-	"attacker fails to impersonate delegate signer": test(async() => {
-		const goodId = deriveId(generateSecret())
-		const badSecret = generateSecret()
-		const delegate: Delegate = {
-			...signDelegate(badSecret, { // actually signed by bad guy
+	"verify delegate": {
+		"reject expired delegates": test(async() => {
+			const secret = generateSecret()
+			const expiresAt = 12_000
+			const scope = scopes.login
+			const delegate = signDelegate(secret, {issuer, audience, expiresAt, scope})
+			expect(() => verifyDelegate(delegate, {atTime: 11_000, ...allowed})).not.throws()
+			expect(() => verifyDelegate(delegate, {atTime: 13_000, ...allowed})).throws()
+			expect(() => verifyDelegate(delegate, {atTime: 12_000, ...allowed})).throws()
+		}),
+
+		"impersonator rejected": test(async() => {
+			const goodId = deriveId(generateSecret())
+			const badSecret = generateSecret()
+			const delegate: Delegate = {
+				...signDelegate(badSecret, { // actually signed by bad guy
+					issuer,
+					audience,
+					scope: scopes.login,
+					expiresAt: time.future.hours(1),
+				}),
+				signedBy: goodId, // pretending to be signed by good guy
+			}
+			expect(() => verifyDelegate(delegate, {...allowed})).throws()
+		}),
+
+		"audience required": test(async() => {
+			const delegate = signDelegate(generateSecret(), {
 				issuer,
+				audience: undefined as any,
+				scope: scopes.login,
+				expiresAt: time.future.hours(1),
+			})
+			expect(() => verifyDelegate(delegate, {...allowed})).throws()
+		}),
+
+		"issuer required": test(async() => {
+			const delegate = signDelegate(generateSecret(), {
+				issuer: undefined as any,
 				audience,
 				scope: scopes.login,
 				expiresAt: time.future.hours(1),
-			}),
-			signedBy: goodId, // pretending to be signed by good guy
-		}
-		expect(() => verifyDelegate(delegate, {...allowed})).throws()
-	}),
+			})
+			expect(() => verifyDelegate(delegate, {...allowed})).throws()
+		}),
 
-	"delegate can expire": test(async() => {
-		const secret = generateSecret()
-		const expiresAt = 12_000
-		const scope = scopes.login
-		const delegate = signDelegate(secret, {issuer, audience, expiresAt, scope})
-		expect(() => verifyDelegate(delegate, {atTime: 11_000, ...allowed})).not.throws()
-		expect(() => verifyDelegate(delegate, {atTime: 13_000, ...allowed})).throws()
-		expect(() => verifyDelegate(delegate, {atTime: 12_000, ...allowed})).throws()
-	}),
+		"reject bad audience": test(async() => {
+			const delegate = signDelegate(generateSecret(), {
+				issuer,
+				audience: "https://bad.e280.org",
+				scope: scopes.login,
+				expiresAt: time.future.hours(1),
+			})
+			expect(() => verifyDelegate(delegate, {...allowed})).throws()
+		}),
+
+		"reject bad issuer": test(async() => {
+			const delegate = signDelegate(generateSecret(), {
+				issuer: "https://bad.e280.org",
+				audience,
+				scope: scopes.login,
+				expiresAt: time.future.hours(1),
+			})
+			expect(() => verifyDelegate(delegate, {...allowed})).throws()
+		}),
+	},
 })
 
