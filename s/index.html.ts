e739e8e5921cea40d4d7f030394a71a6dbcb3182
Chase Moskal <chasemoskal@gmail.com>
2026-03-07 13:27:59 -0800

    setup: site

---
+++ s/index.html.ts
@@ -0,0 +1,35 @@
+
+import {template, html, socialCard} from "@e280/scute"
+
+export default template(import.meta.url, async orb => html`
+	<!doctype html>
+	<html>
+		<head>
+			<meta charset="utf-8"/>
+			<meta name="viewport" content="width=device-width,initial-scale=1"/>
+			<meta name="darkreader-lock"/>
+			<style>@layer base{html{background:#000}}</style>
+
+			<title>authlocal</title>
+			<link rel="icon" href="/assets/favicon.png"/>
+			<script type="module" src="${orb.hashurl("authority/main.bundle.min.js")}"></script>
+			<style>${orb.inject("authority/styles/layers.css")}</style>
+			<style>${orb.inject("authority/styles/vars.css")}</style>
+			<style>${orb.inject("authority/styles/base.css")}</style>
+			<style>${orb.inject("authority/styles/page.css")}</style>
+
+			${socialCard({
+				themeColor: "#77ff81",
+				siteName: "authlocal.org",
+				title: "authlocal – user-sovereign login system",
+				description: "own your identity.. no emails/passwords/databases.. open-source.. privacy-focused..",
+				image: `https://authlocal.org/assets/favicon.png`,
+			})}
+		</head>
+		<body>
+			<h1>authlocal</h1>
+			<p class=version>v${orb.packageVersion()}</p>
+		</body>
+	</html>
+`)
+
