62c3e787c949ac27b263935767e741ed241f726b
Chase Moskal <chasemoskal@gmail.com>
2026-03-27 19:33:58 -0700

    tweak: validator wording

--- s/core/alco/validation.ts
+++ s/core/alco/validation.ts
@@ -4,11 +4,11 @@ import {validator, deny} from "@e280/stz"
 export const maxNameLength = 32
 
 export const validateName = validator<string>(
-	deny("avoid leading or trailing whitespace", s => s !== s.trim()),
+	deny("bad leading or trailing whitespace", s => s !== s.trim()),
 	deny("too small", s => s.length < 1),
 	deny("too big", s => s.length > maxNameLength),
-	deny("avoid consecutive spaces", s => /[ ]{2,}/u.test(s)),
-	deny("avoid weird whitespace", s => /\s/.test(s.replaceAll(" ", ""))),
-	deny("avoid unicode control chars", s => /\p{Z}\p{C}/u.test(s.replaceAll(" ", ""))),
+	deny("bad consecutive spaces", s => /[ ]{2,}/u.test(s)),
+	deny("bad weird whitespace", s => /\s/.test(s.replaceAll(" ", ""))),
+	deny("bad unicode control chars", s => /\p{Z}\p{C}/u.test(s.replaceAll(" ", ""))),
 )
 
