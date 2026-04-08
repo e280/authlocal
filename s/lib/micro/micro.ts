7af143a19377271a52326286c2c16e42e9bc1dc5
Chase Moskal <chasemoskal@gmail.com>
2026-04-08 02:08:18 -0700

    tweak: syntax

--- s/lib/micro/micro.ts
+++ s/lib/micro/micro.ts
@@ -13,11 +13,14 @@ export type MicroData = {
 const version = 0x01
 
 export function microSign(secret: Secret, data: MicroData) {
-	const message = new Uint8Array([version, ...pack(
-		i64(Math.floor(data.expiresAt / 1000)),
-		txt.toBytes(data.audience),
-		data.payload,
-	)])
+	const message = new Uint8Array([
+		version,
+		...pack(
+			i64(Math.floor(data.expiresAt / 1000)),
+			txt.toBytes(data.audience),
+			data.payload,
+		),
+	])
 	const signature = signBytes(secret, message)
 	return `${base58.fromBytes(message)}_${base58.fromBytes(signature)}`
 }
