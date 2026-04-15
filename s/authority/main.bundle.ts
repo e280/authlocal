a019bdabb5441df223354471265530f1c12e77c0
Chase Moskal <chasemoskal@gmail.com>
2026-04-14 17:10:20 -0700

    remove: banner

--- s/authority/main.bundle.ts
+++ s/authority/main.bundle.ts
@@ -2,7 +2,6 @@
 import {html} from "lit"
 import {dom, light} from "@e280/sly"
 import {Bank} from "./sys/bank.js"
-import {Banner} from "./views/banner/view.js"
 import {makeHashRouter} from "./routing/hash-router.js"
 
 const bank = await Bank.init()
@@ -10,21 +9,10 @@ const router = makeHashRouter(bank)
 
 router.startAtHome()
 
-const App = light(() => {
-	return html`
-		${Banner({
-			zone: router.$zone(),
-			gotoList: () => router.go.list(),
-			gotoCreate: () => router.go.create(),
-			gotoRecovery: () => router.go.recovery(),
-		})}
-
-		${router.$content() ?? html`
-			<h2>404 not found.</h2>
-			<button data-vibe="naked lame" @click="${router.go.home}">home</button>
-		`}
-	`
-})
+const App = light(() => router.$content() ?? html`
+	<h2>404 not found.</h2>
+	<button data-vibe="naked lame" @click="${router.go.home}">home</button>
+`)
 
 dom.render(dom("main"), App())
 
