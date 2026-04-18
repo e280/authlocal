68e1e391284e3f328211eec2186338d97b57ab32
Chase Moskal <chasemoskal@gmail.com>
2026-04-18 02:28:13 -0700

    export: micro in lib

--- s/lib/index.ts
+++ s/lib/index.ts
@@ -3,6 +3,7 @@ export * from "./alco/index.js"
 export * from "./cryp/index.js"
 export * from "./ergo/index.js"
 export * from "./tok/index.js"
+export * from "./micro/index.js"
 
 export * from "./utils/err.js"
 
---
+++ s/lib/micro/index.ts
@@ -0,0 +1,6 @@
+
+export * from "./fns/decode.js"
+export * from "./fns/sign.js"
+export * from "./fns/verify.js"
+export * from "./types/data.js"
+
