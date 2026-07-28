6ae08b96d8f294d1b214f08f3a960f2125dd9bc0
Chase Moskal <chasemoskal@gmail.com>
2026-07-28 01:11:22 -0700

    rework: popup algo

--- s/lib/protocol/auth.ts
+++ s/lib/protocol/auth.ts
@@ -1,12 +1,16 @@
 
-import {ev, nap} from "@e280/stz"
 import {signal} from "@e280/strata"
+import {Portal} from "@e280/renraku"
+import {defer, ev, nap} from "@e280/stz"
+import {recvPorts, webAutoTransfer} from "@e280/renraku/web"
+
 import {User} from "./user.js"
+import {consts} from "../../consts.js"
+import {Delegate} from "../core/alco/types.js"
 import {openPopup} from "./parts/open-popup.js"
-import {AuthOptions, SessionOptions} from "./types.js"
 import {isSessionValid} from "./parts/is-session-valid.js"
-import {askForDelegates} from "./parts/ask-for-delegates.js"
 import {sessionPetitions} from "./parts/session-petitions.js"
+import {AuthOptions, DelegatorApi, SessionOptions} from "./types.js"
 import {defaultifyAuthOptions} from "./parts/defaultify-auth-options.js"
 
 /** auth facility for logging in and out. */
@@ -49,9 +53,34 @@ export class Auth {
 
 	/** ask for a new login from the delegator */
 	async loginViaPopup(options: Partial<SessionOptions> = {}) {
+		if (window.crossOriginIsolated)
+			throw new Error("popup flow prohibited by window.crossOriginIsolated")
+
 		const popup = openPopup("auth", this.#options.delegatorUrl)
 		const delegatorOrigin = new URL(this.#options.delegatorUrl, window.location.href).origin
-		const [auth, crypt] = await askForDelegates(popup, delegatorOrigin, sessionPetitions(options))
+		const petitions = sessionPetitions(options)
+		const deferred = defer<Delegate[]>()
+
+		const stop = recvPorts({
+			from: popup,
+			fromOrigin: delegatorOrigin,
+			topic: consts.namespace,
+			onPort: port => {
+				const portal = new Portal<DelegatorApi>({
+					port,
+					timeout: Infinity,
+					autoTransfer: webAutoTransfer,
+				})
+				deferred.entangle(portal.remote.v1.requestDelegates(petitions))
+					.finally(() => {
+						portal.close()
+						popup.close()
+						stop()
+					})
+			},
+		})
+
+		const [auth, crypt] = await deferred.promise
 		const user = new User({auth, crypt})
 		await this.#options.cubby.set(user.session)
 		this.#$user(user)
