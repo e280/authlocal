3080c7bdfbb528c2514972691dbc55c9d9c0775a
Chase Moskal <chasemoskal@gmail.com>
2026-08-01 01:51:19 -0700

    restore: demo page

--- s/demo/demo.bundle.ts
+++ s/demo/demo.bundle.ts
@@ -2,10 +2,8 @@
 import {dom} from "@e280/sly"
 import {Auth} from "../lib/protocol/auth.js"
 import {Widget} from "../lib/ui/views/widget/view.js"
-import { MockAuth } from "../lib/index.js"
 
-// const auth = new Auth({delegatorUrl: "../"})
-const auth = new MockAuth()
+const auth = new Auth({delegatorUrl: "../"})
 await auth.remember()
 
 dom.render(dom(".session"), Widget(auth))
